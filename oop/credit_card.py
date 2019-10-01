01a356fc8c00050c8256dbf371dc8d81f6afa0e6
adavia <andresi.davia@gmail.com>
2019-09-30 22:32:57 -0500

    Add inherited PredatoryCreditCard class

--- oop/credit_card.py
+++ oop/credit_card.py
@@ -52,14 +52,52 @@ class CreditCard:
         """Process customer payment that reduces balance."""
         self._balance -= amount
 
+class PredatoryCreditCard(CreditCard):
+    """An extension to CreditCard that compounds interest and fees."""
+
+    def __init__(self, customer, bank, account, limit, apr):
+        """
+        Create a new predatory credit card instance.
+
+        The initial balance is zero.
+
+        customer the name of the customer (e.g., John Bowman )
+        bank     the name of the bank (e.g., California Savings )
+        account  the acount identifier (e.g., 5391 0375 9387 5309 )
+        limit    credit limit (measured in dollars)
+        apr      annual percentage rate (e.g., 0.0825 for 8.25% APR)
+        """
+
+        super().__init__(customer, bank, account, limit)
+        self._apr = apr
+
+    def charge(self, price):
+        """
+        Charge given price to the card, assuming sufficient credit limit.
+        Return True if charge was processed.
+        Return False and assess 5 fee if charge is denied.
+        """
+        success = super().charge(price)
+        if not success:
+            self._balance += 5
+        return success
+    
+    def process_month(self):
+        """Assess monthly interest on outstanding balance."""
+        if self._balance > 0:
+            # if positive balance, convert APR to monthly multiplicative factor
+            monthly_factor = pow(1 + self._apr, 1/12)
+            self._balance *= monthly_factor
+
+
 if __name__ == '__main__':
     wallet = []
     wallet.append(CreditCard('John Bowman', 'California Saving', 
         '3243 2345 5243 6453', 2500))
     wallet.append(CreditCard('John Bowman', 'California Federal', 
         '8924 2345 2345 6453', 3500))
-    wallet.append(CreditCard('John Bowman', 'California Finance', 
-        '3243 2345 5243 6453', 5000))
+    wallet.append(PredatoryCreditCard('John Bowman', 'California Finance', 
+        '3243 2345 5243 6453', 5000, 9))
 
     for val in range(1, 17):
         wallet[0].charge(val)
